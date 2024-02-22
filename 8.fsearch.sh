cd890faa13ae927a8b5e1baede8686689ce51fe0
martinx73 <35742367+martinx73@users.noreply.github.com>
2024-02-21 21:55:27 -0500

    Add files via upload

---
+++ 8.fsearch.sh
@@ -0,0 +1,1 @@
+git clone https://github.com/cboxdoerfer/fsearch.git && cd fsearch/ && meson builddir && ninja -C builddir install
---
+++ 9.otros.sh
@@ -0,0 +1,5 @@
+sudo apt install unrar && 
+sudo dpkg -i /mnt/DATA/Downloads/LibreOffice24_2/LibreOffice_24.2.0.3_Linux_x86-64_deb/DEBS/*.deb && 
+sudo dpkg -i /mnt/DATA/Downloads/LibreOffice24_2/DEBS/*.deb && 
+cd /mnt/DATA/Downloads/Soft/ && sudo chmod +x JDownloader2Setup_unix_nojre.sh && ./JDownloader2Setup_unix_nojre.sh && cd ~ && 
+cd /mnt/DATA/Downloads/Soft/ && sudo dpkg -i vdhcoapp-linux-x86_64.deb && cd ~
