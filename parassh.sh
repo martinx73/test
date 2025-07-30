0795a2f774afa707e2078a0ff2d639d6b416c259
martinx73 <35742367+martinx73@users.noreply.github.com>
2025-07-30 11:33:17 -0500

    Add files via upload

---
+++ parassh.sh
@@ -0,0 +1,11 @@
+#SSH para SERVIDOR
+sudo apt install libpam-google-authenticator ufw -y && sudo ufw allow ssh && sudo ufw enable
+sudo tee /etc/pam.d/sshd < /dev/null
+echo 'auth required pam_google_authenticator.so' | sudo tee -a /etc/pam.d/sshd
+sudo sed -i '62c\KbdInteractiveAuthentication yes' /etc/ssh/sshd_config
+echo 'ChallengeResponseAuthentication yes' | sudo tee -a /etc/ssh/sshd_config
+echo 'PasswordAuthentication no' | sudo tee -a /etc/ssh/sshd_config
+echo 'PubkeyAuthentication yes' | sudo tee -a /etc/ssh/sshd_config
+echo 'AuthenticationMethods publickey,keyboard-interactive' | sudo tee -a /etc/ssh/sshd_config
+google-authenticator
+sudo reboot
