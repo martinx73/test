e2c09adf50419b089a0cc5effcebf4b23421e68c
martinx73 <35742367+martinx73@users.noreply.github.com>
2025-08-02 12:41:41 -0500

    Add files via upload

--- jul2025.sh
+++ jul2025.sh
@@ -1,10 +1,12 @@
-sudo apt install gparted curl ufw freerdp2-x11 -y && sudo ufw default deny incoming && sudo ufw default allow outgoing && sudo ufw enable
+sudo apt install default-jdk gparted curl ufw freerdp2-x11 -y && sudo ufw default deny incoming && sudo ufw default allow outgoing && sudo ufw enable
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo tee /etc/apt/keyrings/sublimehq-pub.asc > /dev/null
 echo -e 'Types: deb\nURIs: https://download.sublimetext.com/\nSuites: apt/stable/\nSigned-By: /etc/apt/keyrings/sublimehq-pub.asc' | sudo tee /etc/apt/sources.list.d/sublime-text.sources
 sudo apt install software-properties-common -y && sudo apt-add-repository contrib non-free -y
 curl -fSsL https://dl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor | sudo tee /usr/share/keyrings/google-chrome.gpg >> /dev/null && echo deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main | sudo tee /etc/apt/sources.list.d/google-chrome.list
+echo 'deb http://download.opensuse.org/repositories/home:/smplayerdev/Debian_12/ /' | sudo tee /etc/apt/sources.list.d/home:smplayerdev.list
+curl -fsSL https://download.opensuse.org/repositories/home:smplayerdev/Debian_12/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_smplayerdev.gpg > /dev/null
 sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
-sudo apt install unrar google-chrome-stable sublime-text -y
+sudo apt install smplayer unrar google-chrome-stable sublime-text -y
 curl -fsS https://dl.brave.com/install.sh | sh
 sudo mkdir /mnt/data/
 sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
