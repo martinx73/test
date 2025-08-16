64acb9e22150a3f598fac55a00818cc01c8de9a3
martinx73 <35742367+martinx73@users.noreply.github.com>
2025-08-16 12:53:15 -0500

    Add files via upload

--- parassh.sh
+++ parassh.sh
@@ -8,4 +8,3 @@ echo 'PasswordAuthentication no' | sudo tee -a /etc/ssh/sshd_config
 echo 'PubkeyAuthentication yes' | sudo tee -a /etc/ssh/sshd_config
 echo 'AuthenticationMethods publickey,keyboard-interactive' | sudo tee -a /etc/ssh/sshd_config
 google-authenticator
-sudo reboot
