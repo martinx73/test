894db1a1141341309080c456885c73365b4750ed
martinx73 <35742367+martinx73@users.noreply.github.com>
2025-09-28 20:30:20 -0500

    Update jul2025ultraTrixie.sh

--- jul2025ultraTrixie.sh
+++ jul2025ultraTrixie.sh
@@ -1,4 +1,4 @@
-sudo apt install curl
+sudo apt install curl -y
 sudo sed -i 's/non-free-firmware/non-free-firmware contrib non-free/g' /etc/apt/sources.list
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo tee /etc/apt/keyrings/sublimehq-pub.asc > /dev/null
 echo -e 'Types: deb\nURIs: https://download.sublimetext.com/\nSuites: apt/stable/\nSigned-By: /etc/apt/keyrings/sublimehq-pub.asc' | sudo tee /etc/apt/sources.list.d/sublime-text.sources
