6d9e8f27a9463c818ecb945f7af41c4cef09dbe5
martinx73 <35742367+martinx73@users.noreply.github.com>
2025-11-14 22:08:35 -0500

    Add files via upload

--- x01.jul2025beast.sh
+++ x01.jul2025beast.sh
@@ -2,7 +2,6 @@ apt install software-properties-common bleachbit mc default-jdk gparted curl ntf
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | tee /etc/apt/keyrings/sublimehq-pub.asc > /dev/null
 echo -e 'Types: deb\nURIs: https://download.sublimetext.com/\nSuites: apt/stable/\nSigned-By: /etc/apt/keyrings/sublimehq-pub.asc' | tee /etc/apt/sources.list.d/sublime-text.sources
 #sed -i 's/non-free-firmware/non-free-firmware contrib non-free/g' /etc/apt/sources.list
-
 curl -fSsL https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor | tee /usr/share/keyrings/google-chrome.gpg >> /dev/null && echo deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main | tee /etc/apt/sources.list.d/google-chrome.list
 #echo 'deb http://download.opensuse.org/repositories/home:/smplayerdev/Debian_13/ /' | tee /etc/apt/sources.list.d/home:smplayerdev.list
 #curl -fsSL https://download.opensuse.org/repositories/home:smplayerdev/Debian_13/Release.key | gpg --dearmor | tee /etc/apt/trusted.gpg.d/home_smplayerdev.gpg > /dev/null
