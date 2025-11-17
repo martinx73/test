69e55f4975b86a3ff7315bd21a85d23ee5e9c11f
martinx73 <35742367+martinx73@users.noreply.github.com>
2025-11-17 14:43:08 -0500

    Add files via upload

--- x03.nvidiadriver.sh
+++ x03.nvidiadriver.sh
@@ -36,7 +36,28 @@
 
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 # #https://medium.com/codex/install-nvidia-drivers-cuda-on-debian-12-bookworm-nvidia-smi-69d2980247c6
-sudo apt install -y nvidia-driver firmware-misc-nonfree
-sudo apt install -y nvidia-driver nvidia-smi nvidia-settings
-sudo systemctl set-default graphical.target
+
+touch /etc/modprobe.d/blacklist-nouveau.conf
+echo 'blacklist nouveau' | sudo tee -a /etc/modprobe.d/blacklist-nouveau.conf
+echo 'options nouveau modeset=0' | sudo tee -a /etc/modprobe.d/blacklist-nouveau.conf
+sudo update-initramfs -u
+sudo reboot
+sudo apt install build-essential dkms linux-headers-$(uname -r) -y
+sudo apt install software-properties-common -y
+sudo dpkg --add-architecture i386
+sudo apt update
+sudo apt install firmware-misc-nonfree -y
+wget https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/cuda-keyring_1.1-1_all.deb
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
+sudo apt update
+sudo apt install nvidia-driver -y
+sudo reboot
+sudo apt install cuda-12-4 -y
+echo 'export PATH=/usr/local/cuda-12.4/bin${PATH:+:${PATH}}' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=/usr/local/cuda-12.4/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}' >> ~/.bashrc
+echo 'export CUDA_HOME=/usr/local/cuda-12.4' >> ~/.bashrc
+source ~/.bashrc
+#sudo apt install -y nvidia-driver firmware-misc-nonfree
+#sudo apt install -y nvidia-driver nvidia-smi nvidia-settings
+#sudo systemctl set-default graphical.target
 sudo reboot
