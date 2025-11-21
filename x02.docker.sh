67175b630c43fa456dfc77a8366fd1f689f7b3fb
martinx73 <35742367+martinx73@users.noreply.github.com>
2025-11-21 18:23:33 -0500

    Add files via upload

--- x02.docker.sh
+++ x02.docker.sh
@@ -11,3 +11,4 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh ./get-docker.sh
 sudo docker volume create portainer_data
 sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:lts
+sudo systemctl set-default graphical.target
