afc59dc637db59304e18efe35ffc340e2bb3353f
martinx73 <35742367+martinx73@users.noreply.github.com>
2025-11-21 18:16:41 -0500

    Add files via upload

--- x04.cuda.sh
+++ x04.cuda.sh
@@ -3,20 +3,25 @@ sudo dpkg -i cuda-keyring_1.1-1_all.deb
 sudo apt-get update
 sudo apt-get install cuda-toolkit-12-4 -y
 sudo apt-get install cuda-drivers -y
-sudo apt-get install nvidia-cuda-dev -y
+#sudo apt-get install nvidia-cuda-dev -y
 # Agrega esto a tu ~/.bashrc:
-echo 'export PATH=/usr/local/cuda/bin:$PATH' >> /home/martinx73/.bashrc
-echo 'export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH' >> /home/martinx73/.bashrc
-echo 'export CUDA_HOME=/usr/local/cuda' >> /home/martinx73/.bashrc
+#echo 'export PATH=/usr/local/cuda/bin:$PATH' >> /home/martinx73/.bashrc
+#echo 'export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH' >> /home/martinx73/.bashrc
+#echo 'export CUDA_HOME=/usr/local/cuda' >> /home/martinx73/.bashrc
 # LUEGO
-source ~/.bashrc
-curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey \
-    | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
-curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list \
-    | sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' \
-    | sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+#source ~/.bashrc
+#curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey \
+#    | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
+#curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list \
+#    | sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' \
+#    | sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+#sudo apt-get update
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 sudo apt-get update
-sudo apt-get install nvidia-container-toolkit -y
+sudo apt-get install -y nvidia-container-toolkit
 sudo nvidia-ctk runtime configure --runtime=docker
 sudo systemctl restart docker
 sudo docker run --rm --gpus all nvidia/cuda:12.3.2-base-ubuntu22.04 nvidia-smi
